32c9dfe44d67e0406741e3e898d55b7958620837
SAMEHASI <asi.sameh@gmail.com>
2020-04-15 14:39:48 +0300

    create tables + db scripts

---
+++ Garage/DBScripts/FillDb.sql
@@ -0,0 +1,6 @@
+use [D:\WORK\NORA\WORKSPACE\NORA_WORK\GARAGE\GARAGE.MDF]
+
+
+
+DELETE FROM [DBO].[USER];
+insert into dbo.[user] (idusr,Password,firstname,lastname,phone ,datestart,roleusr)values(1,'1','Nora','Rayyan','0504545455','15',1)
---
+++ Garage/DBScripts/init_db.ps1
@@ -0,0 +1,23 @@
+﻿
+
+#Action of connecting to the Database and executing the query and returning results if there were any.
+$conn=New-Object System.Data.SqlClient.SQLConnection
+$db_path = "d:\work\Nora\workspace\nora_work\Garage\Garage.mdf"
+$connectionString = "Data Source=.\SQLExpress;Integrated Security=true; AttachDbFilename=$db_path;User Instance=true"
+
+$Query = "select * from [dbo].[user]"
+
+
+$conn.ConnectionString=$ConnectionString
+$conn.Open()
+
+
+$sqltoexecute = Get-Content("d:\work\Nora\workspace\nora_work\Garage\DBScripts\FillDb.sql");
+
+$cmd=New-Object system.Data.SqlClient.SqlCommand($sqltoexecute,$conn)
+$cmd.CommandTimeout=$QueryTimeout
+$ds=New-Object system.Data.DataSet
+$da=New-Object system.Data.SqlClient.SqlDataAdapter($cmd)
+[void]$da.fill($ds)
+$conn.Close()
+$ds.Tables
